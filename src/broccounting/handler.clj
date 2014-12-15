9b1ba93ca87b72dce13464c45e5935833c5c160c
Ilya Beda <ir4y.ix@gmail.com>
2014-12-15 17:12:13 +0700

    Sessions draft

--- src/broccounting/handler.clj
+++ src/broccounting/handler.clj
@@ -1,6 +1,9 @@
 (ns broccounting.handler
   (:require [compojure.core :refer [defroutes routes]]
+
             [ring.middleware.resource :refer [wrap-resource]]
+            [ring.middleware.session :refer [wrap-session]]
+            [ring.middleware.session.cookie :refer [cookie-store]]
             [ring.middleware.file-info :refer [wrap-file-info]]
             [hiccup.middleware :refer [wrap-base-url]]
             [compojure.handler :as handler]
@@ -20,4 +23,5 @@
 (def app
   (-> (routes home-routes app-routes)
       (handler/site)
+      (wrap-session {:store (cookie-store)})
       (wrap-base-url)))
--- src/broccounting/routes/home.clj
+++ src/broccounting/routes/home.clj
@@ -1,33 +1,41 @@
 (ns broccounting.routes.home
   (:require [compojure.core :refer :all]
+            [ring.util.response :refer response]
             [broccounting.views.layout :as layout]
             [clj-http.client :as client]))
 
-(defn home []
-  (layout/common [:h1 "Hello World!"]))
-
-(defn tasks []
+(defn home [request]
   (layout/common 
-      [:div
-        [:h3 "Result"]
-        [:form {:method "POST"}
-         [:input {:name "login"}]
-         [:input {:name "password" :type "password"}]
-         [:input {:type "submit"}]]]))
+    [:div
+      [:h1 "Hello World!"]
+      (str request)]))
 
-(defn tasks-login [login password]
+(defn tasks [session]
+  (let [id (:jsessionid session)]
+    (if id
+      (layout/common [:h3 (str "Hello " id)])
+      (layout/common 
+          [:div
+            [:h3 "Result"]
+            [:form {:method "POST"}
+             [:input {:name "login"}]
+             [:input {:name "password" :type "password"}]
+             [:input {:type "submit"}]]])))
+
+(defn tasks-login [login password session]
   (let [connect-result (client/post "http://bro.myjetbrains.com/youtrack/rest/user/login"
                                     {:throw-exceptions false
                                      :form-params {:login login
                                                    :password password}})]
-    (layout/common 
-      (if (= (:body connect-result) "ok")
-        "ok"
-        (:body connect-result)))))
-
+    (-> (response (layout/common
+                    (if (= (:body connect-result) "<login>ok</login>")
+                      (let [JSESSIONID (:value (get (:cookies connect-result) "JSESSIONID"))]
+                        (str "JSESSIONID=" JSESSIONID))
+                        (assoc session :jsessionid JSESSIONID)
+                      connect-result)))
+        (assoc :session session))))
 
 (defroutes home-routes
-  (GET "/" [] (home))
-  (GET "/tasks" [] (tasks))
-  (POST "/tasks" [login password] (tasks-login login password)))
-
+  (GET "/" request (home request))
+  (GET "/tasks" [:as {session :session}] (tasks session))
+  (POST "/tasks" [login password :as {session :session}] (tasks-login login password session)))
