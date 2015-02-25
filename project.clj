2d98afbefac613f0efac716def8515c107eeaa35
Ilya Beda <ir4y.ix@gmail.com>
2015-02-25 13:13:29 +0700

    Add elba api

--- project.clj
+++ project.clj
@@ -6,8 +6,14 @@
                  [hiccup "1.0.5"]
                  [ring-server "0.3.1"]
                  [clj-time "0.9.0"]
+                 [cheshire "5.4.0"]
+                 [clj-http "1.0.1"]
+                 [cljfmt "0.1.7"]
+                 [jonase/eastwood "0.2.1"]
                  [org.clojure/data.csv "0.1.2"]]
   :plugins [[lein-ring "0.8.12"]
+            [lein-cljfmt "0.1.7"]
+            [jonase/eastwood "0.2.1"]
             [lein-cloverage "1.0.2"]]
   :ring {:handler broccounting.handler/app
          :init broccounting.handler/init
@@ -19,6 +25,4 @@
     {:open-browser? false, :stacktraces? false, :auto-reload? false}}
    :dev
    {:dependencies [[ring-mock "0.1.5"] 
-                   [ring/ring-devel "1.3.1"]
-                   [clj-http "1.0.1"]
-                   ]}})
+                   [ring/ring-devel "1.3.1"]]}})
---
+++ src/broccounting/elba.clj
@@ -0,0 +1,31 @@
+(ns broccounting.elba
+  (:require [clj-http.client :as client]
+            [cheshire.core :refer :all]
+            [clj-time.core :as t]
+            [clj-time.format :as f]
+            [ring.util.codec :refer [url-encode]]))
+
+(defn create-bill [login password bill]
+  (client/post 
+    "https://elba.kontur.ru/API/CreateBill.ashx"
+    {:body (generate-string bill)
+     :throw-exceptions false
+     :headers {"X-Login"    (url-encode login)
+               "X-Password" (url-encode password)}}))
+
+(def build-bill [name contractor items]
+  {
+  :Number name
+  :Date (f/unparse (f/formatters :date-time) (t/now)) ;;"2014-02-07T00:00:00.000Z",
+  :WithNds false
+  :SumsWithNds false
+  :Contractor {:Name contractor}
+  :Items items})
+
+(def build-item [name quantity price]
+  {:ProductName name
+   :UnitName "час"
+   :Quantity quantity
+   :Price price
+   :PriceWithoutNds price
+   :Sum (* quantity price)})
