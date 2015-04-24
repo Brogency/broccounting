1fcc01bd494720e8322a3e2357b72ed5866a7dc8
Ilya Beda <ir4y.ix@gmail.com>
2015-04-24 09:51:39 +0700

    BRO-98 cover broccounting.handler

--- test/broccounting/test/handler.clj
+++ test/broccounting/test/handler.clj
@@ -1,10 +1,12 @@
 (ns broccounting.test.handler
  (:use clojure.test
-       broccounting.handler))
-
+       broccounting.handler)
+ (:require [broccounting.youtrack :refer [*youtrack-entrypoint*
+                                          *request-cookie-store*]]))
 
 
 (deftest  test-youtrack-middleware
+
   (testing "redirect if youtrack not in session "
     (let [handler (youtrack-middleware (fn [request] request))
           response (handler {:session {}})]
@@ -12,6 +14,7 @@
       (is (= (:status response) 302))
       (is (= (:headers response) {"Location" "/login"}))
       (is (= (:body response) ""))))
+
   (testing "login and css acceptable without youtrack session varaible"
      (let [handler (youtrack-middleware (fn [request] request))
            login-response (handler {:session {}
@@ -22,4 +25,12 @@
               {:uri "/login"}))
        (is (= (dissoc style-response :session)
               {:uri "/css/screen.css"}))))
-  (testing "all page context have youtrack entrypoint and cookie-storage"))
+
+  (testing "all page context have youtrack entrypoint and cookie-storage"
+      (let [youtrack-url "http://bro.myjetbrains.com/youtrack/rest"
+            handler (youtrack-middleware 
+                      (fn [request]
+                        (is (= *youtrack-entrypoint* youtrack-url))
+                        (is (not (nil? *request-cookie-store*)))
+                        request))]
+           (handler {:session {:youtrack youtrack-url} :uri "/login"}))))
